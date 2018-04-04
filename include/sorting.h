f2fcc5b3b882eec5d2b95148054b9aa03a1603a2
Vítor Xoteslem <vitorcx010@gmail.com>
2018-04-04 04:52:56 -0300

    Adding structure of some methods

---
+++ include/sorting.h
@@ -0,0 +1,11 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+class Sorting{
+  private:
+  public:
+    static void quick_sort(int array[], int size);
+    static void merge_sort(int array[], int size);
+};
+
+#endif
---
+++ src/sorting.cpp
@@ -0,0 +1,11 @@
+#include "sorting.h"
+
+void
+Sorting::quick_sort(int array[], int size){
+
+}
+
+void
+Sorting::merge_sort(int array[], int size){
+  
+}
