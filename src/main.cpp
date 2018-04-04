931b8e1c786bb6e8cabb49509a43f13d7d569e15
Vítor Xoteslem <vitorcx010@gmail.com>
2018-04-04 09:00:56 -0300

    Adding functions to generate and print arrays

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,30 @@
 #include <iostream>
 #include "sorting.h"
 
-int main(){
+#define LENGTH 100
+#define START 0
+#define END LENGTH-1
 
+int* generate_array(int size){
+	int* array = new int[size];
+	for(int i=0; i<size; i++){
+		array[i] = rand()% (2*size);
+	}
+	return array;
+}
+
+void print_array(int array[], int size){
+	std::cout << "i | array[i]" << std::endl;
+	for(int i=0; i<size; i++){
+		std::cout << i << " | " << array[i] << std::endl;
+	}
+}
+
+int main(){
+	/*int* array = generate_array(LENGTH);
+	print_array(array, LENGTH);
+	Sorting::quick_sort(array, START, END);
+	print_array(array, LENGTH);
+	*/
 	return 0;
 }
